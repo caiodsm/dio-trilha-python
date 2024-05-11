ac386bc40bcf6d7928a344233dc4ae217f91dfed
caiuzinn <caiomirand@gmail.com>
2024-05-11 18:12:06 -0300

    trilha de python e exercicio

---
+++ primeiro-exercicio/primeiro_programa.py
@@ -0,0 +1,8 @@
+print("hello world!")
+
+#retorna lista de nomes no escopo local usar no terminal
+dir()
+
+# traz mais informações e você pode clicar no Q pra sair do que voce entrou
+# e pra sair total é igual o dir ou estar no modo python você so precisa dar ctr-z ou exit()
+help()
